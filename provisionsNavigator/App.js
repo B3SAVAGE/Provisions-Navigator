831c19129ef27bf0fb36d209d8c037da13b7243a
TheCrazyDragonLady <65409156+Dragoness6215@users.noreply.github.com>
2022-03-16 15:41:52 -0400

    Hello World!

--- provisionsNavigator/App.js
+++ provisionsNavigator/App.js
@@ -1,97 +1,17 @@
-import React, { useState } from "react";
-import { View, StatusBar, FlatList } from "react-native";
-import styled from "styled-components";
+
+import React from 'react';
+import { Text, View } from 'react-native';
 
 
 export default function App() {
- const [data, setData] = useState([]);
- return (
-      <ComponentContainer>
-        <View>
-          <StatusBar barStyle="light-content" 
-             backgroundColor="midnightblue" />
-        </View>
-
-        <View>
-          <FlatList
-            data={data}
-           renderItem={({ item }) => (
-              console.log(item)
-            )}
-          />
-
-        </View>
-      </ComponentContainer>
-    );
-}
-
-const ComponentContainer = styled.View`
-  background-color: midnightblue;
-  height: 100%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
-/*
-import React, { useState } from "react";
-import { View, StatusBar, FlatList } from "react-native";
-import styled from "styled-components";
-import AddInput from "./Components/AddInput";
-import TodoList from "./Components/TodoList";
-import Header from "./Components/Header";
-import Empty from "./Components/Empty";
-
-export default function App() {
- const [data, setData] = useState([]);
- const submitHandler = (value) => {
-  setData((prevTodo) => {
-    return [
-      {
-        value: value,
-        key: Math.random().toString(),
-      },
-      ...prevTodo,
-    ];
-  });
-};
-const deleteItem = (key) => {
-  setData((prevTodo) => {
-    return prevTodo.filter((todo) => todo.key != key);
-  });
-};
-
   return (
-    <ComponentContainer>
-      <View>
-        <StatusBar barStyle="light-content" 
-          backgroundColor="midnightblue" />
-      </View>
-
-      <View>
-      
-      <FlatList
-            data={data}
-            ListHeaderComponent={() => <Header />}
-            ListEmptyComponent={() => <Empty />}
-            keyExtractor={(item) => item.key}
-            renderItem={({ item }) => (
-              <TodoList item={item} deleteItem={deleteItem} />
-            )}
-          />
-        <View>
-          <AddInput submitHandler={submitHandler} />
-        </View>
-      </View>
-    </ComponentContainer>
-  );
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
+  )
 }
-
-const ComponentContainer = styled.View`
-  background-color: midnightblue;
-  height: 100%;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-*/
