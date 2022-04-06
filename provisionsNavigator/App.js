f59fbd6f432e87ae9ba1d3ee491f4d52f8255022
Ben Hairston <32179492+B3SAVAGE@users.noreply.github.com>
2022-04-06 11:13:22 -0400

    Progress on async storage so far

--- provisionsNavigator/App.js
+++ provisionsNavigator/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert, TouchableWithoutFeedback, Keyboard, setState } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -13,18 +15,72 @@ export default function App() {
 
   const pressHandler = (key) => {
     setTodos(prevTodos => {
+      removeData(key)
       return prevTodos.filter(todo => todo.key != key);
+      
+      GenerateRand();
     });
   };
 
-  const submitHandler = (text) =>{
-    setTodos((prevTodos) =>{
-      return [
-        {text: text, key:Math.random().toString()},
-        ...prevTodos
-      ]
-    })
+  const removeData = async(text) => {
+    try{
+      AsyncStorage.removeItem(newText)
+      console.log(text + 'has been removed')
+    }catch(err){
+      console.log(err)
+
+    }
   }
+  
+
+  const GenerateRand=()=>{
+    var min = Math.ceil(100);
+    var max = Math.floor(3)
+    var RandomNum = Math.floor(Math.random() * (max - min) + min);
+    return RandomNum;
+  }
+
+  const writeData = async (newText) => {
+    try {
+        AsyncStorage.setItem('todos', JSON.stringify(newText))
+    } catch (err) {
+        console.log(err)
+    }
+  };
+
+
+
+  const submitHandler = async (text) =>{
+    setTodos((prevTodos) =>{
+     var keything = GenerateRand().toString()
+      if(text.length > 0){
+          let newText = {
+            name: text,
+            key: keything,
+          };
+          
+           writeData(newText)
+           console.log(newText)
+  
+      return [
+        {text: text, key:GenerateRand().toString()},
+        ...prevTodos]
+    }
+  })};
+
+  // const printKeys = async()=>{
+  //   try {
+  //     const keys = await AsyncStorage.getAllKeys();
+  //     const result = await AsyncStorage.multiGet(keys);
+  
+  //     return result.map(req => JSON.parse(req)).forEach(console.log);
+  //   } catch (error) {
+  //     console.error(error)
+  //   }
+  // }
+
+  
+  
 
   return (
     <View style={styles.container}>
@@ -34,9 +90,8 @@ export default function App() {
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => (
-              <TodoItem item={item} pressHandler={pressHandler} />
-            )}
+            renderItem={({ item }) => (<TodoItem item={item} pressHandler={pressHandler} />)}
+
           />
         </View>
       </View>
