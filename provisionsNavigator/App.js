51749d709e2d6c9f2d2cb2da36d6f723410f462e
Ben Hairston <32179492+B3SAVAGE@users.noreply.github.com>
2022-04-07 20:55:17 -0400

    cleaning App.js

--- provisionsNavigator/App.js
+++ provisionsNavigator/App.js
@@ -1,19 +1,11 @@
-<<<<<<< Updated upstream
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert, TouchableWithoutFeedback, Keyboard, setState } from 'react-native';
-=======
+
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, FlatList, TextInput, Button, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
->>>>>>> Stashed changes
 import Header from './components/header';
 import TodoItem from './components/todoItem';
 import AddTodo from './components/addTodo';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 
 export default function App() {
   const [todos, setTodos] = useState([]);
@@ -24,92 +16,6 @@ export default function App() {
 
 
   const pressHandler = (key) => {
-<<<<<<< Updated upstream
-    setTodos(prevTodos => {
-      removeData(key)
-      return prevTodos.filter(todo => todo.key != key);
-      
-      GenerateRand();
-    });
-  };
-
-  const removeData = async(text) => {
-    try{
-      AsyncStorage.removeItem(newText)
-      console.log(text + 'has been removed')
-    }catch(err){
-      console.log(err)
-
-    }
-  }
-  
-
-  const GenerateRand=()=>{
-    var min = Math.ceil(100);
-    var max = Math.floor(3)
-    var RandomNum = Math.floor(Math.random() * (max - min) + min);
-    return RandomNum;
-  }
-
-  const writeData = async (newText) => {
-    try {
-        AsyncStorage.setItem('todos', JSON.stringify(newText))
-    } catch (err) {
-        console.log(err)
-    }
-  };
-
-
-
-  const submitHandler = async (text) =>{
-    setTodos((prevTodos) =>{
-     var keything = GenerateRand().toString()
-      if(text.length > 0){
-          let newText = {
-            name: text,
-            key: keything,
-          };
-          
-           writeData(newText)
-           console.log(newText)
-  
-      return [
-        {text: text, key:GenerateRand().toString()},
-        ...prevTodos]
-    }
-  })};
-
-  // const printKeys = async()=>{
-  //   try {
-  //     const keys = await AsyncStorage.getAllKeys();
-  //     const result = await AsyncStorage.multiGet(keys);
-  
-  //     return result.map(req => JSON.parse(req)).forEach(console.log);
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
-
-  
-  
-
-  return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <AddTodo submitHandler={submitHandler} />
-        <View style={styles.list}>
-          <FlatList
-            data={todos}
-            renderItem={({ item }) => (<TodoItem item={item} pressHandler={pressHandler} />)}
-
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
-=======
 
     console.log('Todos BEFORE delete');
     console.log(todos);
@@ -189,7 +95,6 @@ return (
         </TouchableWithoutFeedback>
       );
     }
->>>>>>> Stashed changes
 
 const styles = StyleSheet.create({
   container: {
